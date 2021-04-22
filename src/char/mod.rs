d69e7e387356972860bd67d651d20c2a730fd2f6
WatchDG <baranov@w3m.co>
2021-04-22 17:27:32 +0300

    add char macros

---
+++ src/char/mod.rs
@@ -0,0 +1,35 @@
+#[macro_export]
+macro_rules! rf {
+    () => {
+        0x0d
+    };
+}
+
+#[test]
+fn rf_test() {
+    assert_eq!(rf!(), '\r' as u8);
+}
+
+#[macro_export]
+macro_rules! lf {
+    () => {
+        0x0a
+    };
+}
+
+#[test]
+fn lf_test() {
+    assert_eq!(lf!(), '\n' as u8);
+}
+
+#[macro_export]
+macro_rules! space_bar {
+    () => {
+        0x20
+    };
+}
+
+#[test]
+fn space_bar_test() {
+    assert_eq!(space_bar!(), ' ' as u8);
+}
